8579cf9537a6878018a3f8cb6edd664089a24f44
smithx <smithx@pm.me>
2020-12-19 23:01:29 +0300

    Use CMake to generate VS projects and solutions.

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,5 +1,6 @@
 @echo off
 if "%1" == "" (set arch=x64)
+if "%1" == "x64" (set arch=x64)
 if "%1" == "X64" (set arch=x64)
 if "%1" == "Win64" (set arch=x64)
 
@@ -11,14 +12,14 @@ set source_dir=%~dp0
 set build_dir=%source_dir%\build-%arch%
 
 cd %source_dir%
-cmake -G "Visual Studio 16 2019" -A %arch% -S %source_dir% -B %build_dir% -DCMAKE_INSTALL_PREFIX="%source_dir%\Output"
+cmake -G "Visual Studio 15 2017" -A %arch% -S %source_dir% -B %build_dir% -DCMAKE_INSTALL_PREFIX="%source_dir%\Output"
 
 echo Now you can either build the package
 echo.
-echo 	cmake --build build-%arch% --config Release
-echo	cd build-%arch%
-echo	cpack -G ZIP gitbranch
+echo    cmake --build build-%arch% --config Release
+echo    cd build-%arch%
+echo    cpack -G ZIP gitbranch
 echo.
 echo or open the solution file and start developing
 echo.
-echo 	build-%arch%\gitbranch.sln
+echo    build-%arch%\gitbranch.sln
